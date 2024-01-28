139a389c3b131694958b753cdb42a75ec01fb657
NamTran <phuongnamtran20899@gmail.com>
2024-01-28 15:38:28 +0700

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            gridColumn: {
+            'span-13': 'span 13 / span 13',
+            'span-14': 'span 14 / span 14',
+            'span-15': 'span 15 / span 15',
+            'span-16': 'span 16 / span 16',
+            'span-17': 'span 17 / span 17',
+            'span-18': 'span 18 / span 18',
+            'span-19': 'span 19 / span 19',
+            'span-20': 'span 20 / span 20',
+            'span-21': 'span 21 / span 21',
+            'span-22': 'span 22 / span 22',
+            'span-23': 'span 23 / span 23',
+            'span-24': 'span 24 / span 24',
+        },
+        gridRow: {
+            'span-13': 'span 13 / span 13',
+            'span-14': 'span 14 / span 14',
+            'span-15': 'span 15 / span 15',
+            'span-16': 'span 16 / span 16',
+            'span-17': 'span 17 / span 17',
+            'span-18': 'span 18 / span 18',
+            'span-19': 'span 19 / span 19',
+            'span-20': 'span 20 / span 20',
+            'span-21': 'span 21 / span 21',
+            'span-22': 'span 22 / span 22',
+            'span-23': 'span 23 / span 23',
+            'span-24': 'span 24 / span 24',
+            },
+            gridTemplateColumns: {
+                // Simple 16 column grid
+                '22': 'repeat(22, minmax(0, 1fr))',
+            }
+        }
+    }
+}
